8d59cf127e27f1a240920dc87aaef3438467fd56
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-12-04 01:41:10 -0500

    done

--- photogur/app/models/picture.rb
+++ photogur/app/models/picture.rb
@@ -1,4 +1,10 @@
 class Picture < ActiveRecord::Base
+
+  validates :title, :url, presence: true
+  validates :title, length: {minimum: 3, maximum: 20}
+  validates :url, uniqueness: true
+
+
   has_and_belongs_to_many :tags
   has_many :comments
   # Now we can do these things!
